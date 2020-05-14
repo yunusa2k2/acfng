4b95e368f30e092fadf6cae596332d727f971d56
Yunusa Haruna <yunusa2k2@gmail.com>
2020-05-15 04:41:35 +0800

    updated config database parameters

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
-module.exports = new Sequelize('acfng', 'root', '', {
-    host: 'localhost',
+module.exports = new Sequelize('mozacs6znbnlfqn9', 'mmbd48vz4dw3os9h', 'xrfaljz6288cexcs', {
+    host: 'pqxt96p7ysz6rn1f.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
     dialect: 'mysql',
     operatorsAliases: 0,
 
@@ -11,3 +11,17 @@ module.exports = new Sequelize('acfng', 'root', '', {
         idle: 10000
     },
 });
+
+// const Sequelize = require('sequelize');
+// module.exports = new Sequelize('acfng', 'root', '', {
+//     host: 'localhost',
+//     dialect: 'mysql',
+//     operatorsAliases: 0,
+//
+//     pool: {
+//         max: 5,
+//         min: 0,
+//         acquire: 30000,
+//         idle: 10000
+//     },
+// });
